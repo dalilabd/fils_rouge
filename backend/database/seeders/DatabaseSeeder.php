637a51a2f9a64e15cc4d247dc9716784ef8ac757
Your Name <you@example.com>
2024-10-16 23:59:37 +0200

    he

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -12,13 +12,19 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+    //C:\Program Files\nodejs\
+
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+        $this->call([
+            RolesTableSeeder::class,
+            PermissionsTableSeeder::class,
+        ]);
     }
 }
